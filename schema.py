cc1fd6a0f3496aa76cd9a8c128a47c63a5efd378
Raphael Lacombe <ralac74@ulaval.ca>
2025-05-04 20:25:20 -0400

    Corrections schema

--- schema.py
+++ schema.py
@@ -5,10 +5,8 @@ def schema(sigma, L, f, h, tau, K):
     n = int(L / h)
     x = np.linspace(0, L, n+1)
     
-    # Initialisation de U0 (n-1 composantes, points intérieurs)
     U0 = np.array([f(x[j]) for j in range(1, n)])
 
-    # Construction de la matrice Ah de taille (n-1)x(n-1)
     Ah = np.zeros((n-1, n-1))
     for i in range(n-1):
         Ah[i, i] = -2
@@ -18,11 +16,9 @@ def schema(sigma, L, f, h, tau, K):
             Ah[i, i+1] = 1
     Ah = (sigma / h**2) * Ah
 
-    # Fonction F(t, U) = Ah @ U
     def F(t, U):
         return Ah @ U
 
-    # Appel d'euler_explicite
     t_vals, y_vals = euler_explicite(F, 0, U0, tau, K * tau)
 
     return t_vals, y_vals
